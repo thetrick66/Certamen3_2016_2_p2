78db17bce65dc9d448b6c180daf3c5c58ca5006a
Patricio <patricio.rodriguezg@alumnos.usm.cl>
2016-11-18 22:21:55 -0300

    Certamen 3 p2 Patricio Rodriguez 201130007-6

--- app/src/main/java/cl/telematica/android/certamen3_p2/MyAsyncTaskExecutor.java
+++ app/src/main/java/cl/telematica/android/certamen3_p2/MyAsyncTaskExecutor.java
@@ -27,7 +27,8 @@ public class MyAsyncTaskExecutor {
 
             @Override
             protected String doInBackground(Void... params) {
-                String resultado = new HttpServerConnection().connectToServer("http://www.mocky.io/v2/582f1759260000171165f0b6" + "?data=" + textToSend, 15000);
+                String resultado = new HttpServerConnection().connectToServer("http://10.2.100.132:3000/api/clients/", 15000);
+                //String resultado = new HttpServerConnection().connectToServer("http://10.2.100.132:3000/api/clients/" + ":" + textToSend, 15000);
                 return resultado;
             }
 
